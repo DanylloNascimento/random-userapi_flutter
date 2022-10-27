a1833b2d49baa444fb60753393a98d2b30a45542
DanylloNascimento <pdanyllo7@gmail.com>
2022-10-27 10:55:42 -0300

    ultimas atualizacoes projeto random api ajustes principios solid class check connectivy

--- README.md
+++ README.md
@@ -6,13 +6,30 @@ Aplicativo desenvolvido com o Framework Flutter para consumir a Random User API
 ### Random Users API - https://randomuser.me/api/
 
 ### Plugins Utilizados
-- Provider: Para o gerenciamento de estado
-- Http: Para acessar e trazer os dados da api utilizada
+- provider: Para o gerenciamento de estado
+- http: Para acessar e trazer os dados da api utilizada
+- connectivity_plus: Para teste de conexão com internet
 
 
 ### Screenshots
 
-<img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/users%20male.jpg" height="430em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/users%20female.jpg"  height="430em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/userview%20male.jpg" height="430em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/userview%20female.jpg" height="430em">
+<img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/users%20male.jpg" height="400em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/users%20female.jpg"  height="400em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/userview%20male.jpg" height="400em"> <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/userview%20female.jpg" height="400em">
 
 ### GIF
 <img src="https://github.com/DanylloNascimento/random-userapi_flutter/blob/main/assets/gif.gif" height="560em" width="300">
+
+
+### Perguntas
+
+Ao compartilhar o link do projeto finalizado, pedimos que responda mais 3 perguntas adicionais a seguir:
+
+1) Quais padrões de gerenciamento de estados já utilizou em seus projetos ou que tem bons conhecimentos?
+    Já utilizei provider e getX, mas atualmente tenho bons conhecimentos tenho com Provider, e justamente utilizei neste projeto. 
+
+2) Tem conhecimentos de SOLID e arquitetura limpa? Se sim, já utilizou em algum projeto?
+    Tenho conhecimentos em SOLID e utilizei nos projetos da empresa que estou atualmente e em projetos freelancer
+
+3) Poderia listar e compartilhar projetos em Flutter já realizados?
+    App de vendas externas integrado com sistema desktop utilizando conexão mysql e apis PHP. 
+    App PDV com integração com banco mysql e consumo de apis PHP.
+    App Delivery com toda conexão com Firebase(Auth, Storage, Cloud functions e Cloud Firestore). 
--- lib/controllers/userProvider.dart
+++ lib/controllers/userProvider.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 import 'dart:convert' as convert;
 
-import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:random_api_test/controllers/checkConnectivy.dart';
 import 'package:random_api_test/models/results.dart';
 import 'package:random_api_test/models/user.dart';
 
@@ -14,6 +15,7 @@ class UserState with ChangeNotifier {
   viewState _state = viewState.Done;
   viewState get state => _state;
   late String filterSelected;
+  CheckConnectivy checkConnectivy = CheckConnectivy();
 
   void setState(viewState currentState) {
     _state = currentState;
@@ -25,18 +27,87 @@ class UserState with ChangeNotifier {
     notifyListeners();
   }
 
-  Future getDataApi() async {
-    final url = await http.get(Uri.parse(
-        'https://randomuser.me/api/?results=20&gender=$filterSelected'));
-    randomAPI = RandomAPI.fromJson(convert.jsonDecode(url.body));
-    users = randomAPI.results;
-    notifyListeners();
+  Future getDataApi(BuildContext context) async {
+    if (await checkConnectivy.checkConnectivy()) {
+      try {
+        final url = await http.get(Uri.parse(
+            'https://randomuser.me/api/?results=20&gender=$filterSelected'));
+        randomAPI = RandomAPI.fromJson(convert.jsonDecode(url.body));
+        users = randomAPI.results;
+      } catch (e) {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("Error: Incorrect data"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: const <Widget>[
+                  Text("Check the api link that was used")
+                ],
+              ),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue[900])),
+                  child: const Text("Ok"),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    style: const ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.red)),
+                    child: const Text("Cancel")),
+              ],
+            );
+          },
+        );
+      }
+
+      notifyListeners();
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Error: No connection"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const <Widget>[Text("Check your internet connection")],
+            ),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.blue[900])),
+                child: const Text("Ok"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  style: const ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.red)),
+                  child: const Text("Cancel")),
+            ],
+          );
+        },
+      );
+    }
   }
 
-  loadDataUsers() async {
+  loadDataUsers(BuildContext context) async {
     setState(viewState.IsLoading);
     users = [];
-    await getDataApi();
+    await getDataApi(context);
     setState(viewState.Done);
   }
 }
